a381742b14bdd66b37ddf2dfefe2d330a5e5ddca
AtharPnh <ath.pnh@gmail.com>
2024-10-31 14:22:15 +0800

    Initial commit

---
+++ src/main/java/com/springinaction/tacoman/TacoManApplication.java
@@ -0,0 +1,13 @@
+package com.springinaction.tacoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springinaction/tacoman/TacoManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springinaction.tacoman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
