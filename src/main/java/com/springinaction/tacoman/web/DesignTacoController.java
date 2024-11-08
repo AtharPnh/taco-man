c35562fb44ebe149c76a947e9e147b74cac95f8f
AtharPnh <ath.pnh@gmail.com>
2024-11-08 15:36:21 +0800

    add lesson from chapter 02

---
+++ src/main/java/com/springinaction/tacoman/web/DesignTacoController.java
@@ -0,0 +1,85 @@
+package com.springinaction.tacoman.web;
+
+import com.springinaction.tacoman.entity.Ingredient;
+import com.springinaction.tacoman.entity.Taco;
+import com.springinaction.tacoman.entity.TacoOrder;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("tacoOrder")
+public class DesignTacoController {
+
+    /**
+     * This method will be invoked when a request is handled and will construct a list of Ingredient objects to be
+     * put into the model.
+     *
+     * @param model
+     */
+    @ModelAttribute
+    public void addIngredientsToModel(Model model) {
+
+        List<Ingredient> ingredients = new ArrayList<>(List.of(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+        ));
+
+        Ingredient.Type[] types = Ingredient.Type.values();
+
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.name().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+    }
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
+    }
+
+    @ModelAttribute(name = "tacoOrder")
+    public TacoOrder order() {
+        return new TacoOrder();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    @GetMapping
+    public String showDesignForm() {
+        return "design";
+    }
+
+    @PostMapping
+    public String processTaco(@Valid Taco taco, Errors errors,
+                              @ModelAttribute TacoOrder tacoOrder) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
+    }
+
+}
